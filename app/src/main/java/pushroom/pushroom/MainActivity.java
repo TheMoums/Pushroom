f5c1cce3231a2436ddc0aafc822d0c18520915d1
henri moumal <henri.moumal@student.uclouvain.be>
2017-04-29 13:09:11 +0200

    Initial commit

---
+++ app/src/main/java/pushroom/pushroom/MainActivity.java
@@ -0,0 +1,18 @@
+package pushroom.pushroom;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by henri on 29-04-17.
+ */
+
+public class MainActivity extends AppCompatActivity {
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            //Intent intent = new Intent(getApplicationContext(), WallsList.class);
+            //startActivity(intent);
+        }
+}
